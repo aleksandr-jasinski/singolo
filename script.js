4e0611eab1d4276c9a7bc5714c72ff3a9b084b35
Aleksandr Jasinski <aleksandr.jasinski@gmail.com>
2020-03-26 17:05:59 +0300

    refactor: remake slider again

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const MENU = document.getElementById('menu');
+const BTN_LEFT = document.getElementsByClassName('slider__btn-left');
+const BTN_RIGHT = document.getElementsByClassName('slider__btn-right');
+const SLIDER1 = document.getElementById('slide1');
+const SLIDER2 = document.getElementById('slide2');
 
 MENU.addEventListener('click', (event) => {
   MENU.querySelectorAll('a').forEach(el => el.classList.remove('menu__item_active'));
   event.target.classList.add('menu__item_active');
 })
+
+
